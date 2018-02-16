07505d7a50f2ffc83b636682aa7ae4dd968cf69b
ppescher <ppescher@hotmail.com>
2018-02-16 22:45:37 +0100

    Fix missing return statements

--- src/can_common.cpp
+++ src/can_common.cpp
@@ -153,19 +153,18 @@ void CAN_COMMON::detachCANInterrupt(uint8_t mailBox)
 
 int CAN_COMMON::setRXFilter(uint8_t mailbox, uint32_t id, uint32_t mask, bool extended)
 {
-    _setFilterSpecific(mailbox, id, mask, extended);
+    return _setFilterSpecific(mailbox, id, mask, extended);
 }
 
 int CAN_COMMON::setRXFilter(uint32_t id, uint32_t mask, bool extended)
 {
-    _setFilter(id, mask, extended);
+    return _setFilter(id, mask, extended);
 }
 
 int CAN_COMMON::watchFor() 
 {
     setRXFilter(0, 0, false);
 	return setRXFilter(0, 0, true);
-
 }
 
 //Let a single frame ID through. Automatic determination of extended. Also automatically sets mask
